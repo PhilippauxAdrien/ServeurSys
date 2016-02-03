2950270643e52a6653f2eb30c1b03098a49bc8da
Adrien PHILIPPAUX <adrien.philippaux@etudiant.univ-lille1.fr>
2016-02-03 15:52:21 +0100

    modif du main pour accepter les clients

--- webserver/main.c
+++ webserver/main.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-int main(int argc, char **argv){
-	if(argc > 1 && strcmp(argv[1], "-advice") == 0){
-		printf("Don't Panic!\n");
-		return 42;
+#include "socket.h"
+#include <unistd.h>
+#include <sys/socket.h>
+
+int main(void){
+
+	int socket_serveur = creer_serveur(8080);
+	if(socket_serveur == -1){
+		perror("Création du serveur");
+		return -1;
 	}
-	printf("Need an advice\n");
+	
+	int socket_client ;
+	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\nCe serveur est un serveur test\nDonc s'il ne fonctionne pas encore correctement,\nNe vous inquietez pas\nNous allons régler cela dans les plus brefs délais\nEn attendant, vous pouvez regarder ce joli message défiler\nEnfin, si tout marche bien!" ;
+	
+	while(1){
+	
+	socket_client = accept_client(socket_serveur,NULL,NULL);
+	printf("%d\n",socket_client);
+	if(socket_client == -1){
+		perror("accept");
+		/* traitement d ’ erreur */
+		return -1;
+	}
+
+	/* On peut maintenant dialoguer avec le client */
+	write(socket_client,message_bienvenue,strlen(message_bienvenue));
+	     
+	}
+
 	return 0;
 }
