a96fb3f43d39a06a99005b8f28d075a9d9980778
Adrien PHILIPPAUX <adrien.philippaux@etudiant.univ-lille1.fr>
2016-02-10 15:06:44 +0100

    update mai echo

--- webserver/main.c
+++ webserver/main.c
@@ -11,33 +11,9 @@ int main(void){
 			perror("Création du serveur");
 			return -1;
 		}
-
-	int socket_client ;
-	//const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\nCe serveur est un serveur test\nDonc s'il ne fonctionne pas encore correctement,\nNe vous inquietez pas\nNous allons régler cela dans les plus brefs délais\nEn attendant, vous pouvez regarder ce joli message défiler\nEnfin, si tout marche bien!" ;
-	char buf[4096];
+	
 	while(1){
-	
-		socket_client = accept_client(socket_serveur,NULL,NULL);
-	
-		if(socket_client == -1){
-			perror("accept");
-			/* traitement d ’ erreur */
-			return -1;
-		}
-		/* On peut maintenant dialoguer avec le client */
-		
-		while(1){
-		sleep(1); /* Attente d'une seconde avant l'envoi du message */
-		if(read(socket_client , buf ,sizeof(buf)) == -1){
-			perror("read");
-			break;
-		}
-		if(write(socket_client,buf,strlen(buf)) == -1){
-			perror("write");
-			break;
-			} 
-		}
-		close(socket_client);
+		accept_client(socket_serveur);
 	}
 
 	return 0;
